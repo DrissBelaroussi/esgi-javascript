59a9da0191e4627da79966ea5aa6c61dba546b1a
Driss.Belaroussi <driss.belaroussi@betc.com>
2018-11-27 10:27:04 +0100

    exo 2.1

--- exercice-2/type-check.js
+++ exercice-2/type-check.js
@@ -4,10 +4,14 @@
 function type_check_v1(value , type){
 	if (typeof value === type){
 		return true;
+	} else if(value === null || value === 'undefined' ){
+		if ( value == type ){
+			return true ; 
+		}
 	} else {
-		return false ; 
+		return false ;
 	}
 }
 
-check = type_check_v1 ([1,2], 'Array');
+check = type_check_v1 ('undefined','undefined' );
 console.log(check);
