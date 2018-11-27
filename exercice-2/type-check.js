c40026a244da6940357b896b07b4b606ffd33321
Driss.Belaroussi <driss.belaroussi@betc.com>
2018-11-27 12:12:11 +0100

    exo 2.2

--- exercice-2/type-check.js
+++ exercice-2/type-check.js
@@ -18,5 +18,29 @@ function type_check_v1(value , type){
 	}
 }
 
-check = type_check_v1 (null, 'object' );
+function type_check_v2(value , type){
+	if(value == type.value && type_check_v1(value, type.type) == true){
+		return true;
+	} else if(typeof value == 'object') {
+		return type_check_v1(value, type.type);
+	} else if(type.enum) {
+		for (var i = 0; i < type.enum.length; i++) {
+			if(type.enum[i] === value){
+				return true;
+			} else {
+			return false
+			}
+		}
+	} else {
+		return false;
+	}
+}
+
+check = type_check_v2 ({prop1:1}, {type:"object"});
+console.log(check);
+check = type_check_v2 ("foo", {type:"string" , value:"foo"});
+console.log(check);
+check = type_check_v2 ("bar", {type:"string" , value:"foo"});
+console.log(check);
+check = type_check_v2 (2, {enum:["bob", "boy" , 3]});
 console.log(check);
